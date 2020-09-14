c4cdbb735f52a4edad7307a33fdcb4cc79083844
CPPtrueCoder <andierodin@gmail.com>
2020-09-14 21:15:30 +0300

    Add bindActionCreators from Redux

--- src/index.js
+++ src/index.js
@@ -1,35 +1,32 @@
-import {createStore} from "redux";
+import {createStore, bindActionCreators} from "redux";
+import reducer from './reducer'
+import  {increment, decrement ,reset} from "./actions";
 
 const inc = document.getElementById('inc'),
     dec= document.getElementById('dec'),
     rst= document.getElementById('rst');
-const reducer = (state= 0,action) =>{
-    switch(action.type){
-        case 'INC':
-        return state +1;
-        case 'DEC':
-            return state -1;
-        case 'RST':
-            return state =0;
-        default:
-            return state;
-    }
-
-}
-
-const increment =()=>({type: "INC"});
-const decrement =()=> ({type:'DEC'});
-const reset =()=> ({type:'RST'});
 
 const store = createStore(reducer);
+const {dispatch} = store;
+
+// const bindActionCreator =(creator, dispatch)=>(...params)=>{
+//     dispatch(creator(...params));
+// }
+
+
+const incDispatch =bindActionCreators(increment,dispatch);
+const decDispatch =bindActionCreators(decrement, dispatch)
+const resetDispatch =bindActionCreators(reset,dispatch)
+
+
 inc.addEventListener('click', ()=>{
-    store.dispatch(increment());
+    incDispatch()
 })
 dec.addEventListener('click', ()=>{
-    store.dispatch(decrement());
+  decDispatch()
 })
 rst.addEventListener('click', ()=>{
-    store.dispatch(reset());
+    resetDispatch();
 })
 
 const update = ()=>{
